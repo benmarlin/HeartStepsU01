d252ab04f23d3c9fbf1b2cb74891efec3205076b
Ben Marlin <marlin@cs.umass.edu>
2020-12-03 20:32:50 -0500

    Updated data utils

--- data_utils.py
+++ data_utils.py
@@ -1,12 +1,7 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn
 import os
 import zipfile
-from IPython.display import display, Markdown, Latex
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
 
 study_prefix = "U01 Data/usc-study.u01."
 
@@ -63,39 +58,6 @@ def get_subject_ids(df):
 def get_variables(df): 
   cols = [c for c in list(df.columns) if df.dtypes[c] == np.dtype('float64')]
   return(cols)
-  
-def plot_summary_histograms(df, dd, cols=3, fields=[]):  
-    num_fields = len(dd["categorical_fields"])+len(dd["numerical_fields"])
-    rows = int(np.ceil(num_fields/3))
-    fig, axes = plt.subplots(rows, cols, figsize=(4*3,rows*3))
-    i=0
-    for field in list(df.keys()):
-        if(field in fields or len(fields)==0):
-            if field in dd["categorical_fields"]:
-                this_ax = axes[i//cols,i%cols]
-                df[field].value_counts().plot(kind="bar",ax=this_ax)
-                this_ax.grid(axis='y')
-                this_ax.set_title(field)
-                i=i+1
-            if field in dd["numerical_fields"]: 
-                this_ax = axes[i//cols,i%cols]
-                df[field].hist(figure=fig,ax=this_ax)
-                this_ax.grid(True)
-                this_ax.set_title(field)
-                i=i+1
-    plt.tight_layout()
-    plt.show()
-
-def plot_indifivual_time_series(df,variable,subject_id):
-  this_df = df.xs(subject_id, level=0, axis=0, drop_level=True)
-  this_df[variable].plot(kind='bar', grid=True, figsize=(12,4) )
-  plt.title("Subject %s: %s"%(subject_id,variable))
-  plt.show()
-
-def show_individual_time_series_visualizer(df):
-  sids=get_subject_ids(df)
-  vars=get_variables(df)
-  interact(plot_indifivual_time_series, df=fixed(df), subject_id=sids,variable = vars);
 
 data_dicts = {}
 data_dicts["daily-metrics"]={}
