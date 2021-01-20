523d05b6f9ebc230a03bb3e545d243cdb35f55b4
Ben Marlin <marlin@cs.umass.edu>
2021-01-20 10:54:17 -0500

    Updated report utils to account for participant status

--- report_utils.py
+++ report_utils.py
@@ -13,6 +13,9 @@ from . import data_utils
 
 def morning_survey_response_report(dc):
 
+  participants_df = data_utils.get_participant_info(dc)
+  participants_df=participants_df.set_index("Participant ID")
+
   product = "Morning Survey"
   df=data_utils.load_data(dc, product)
 
@@ -24,25 +27,32 @@ def morning_survey_response_report(dc):
 
   participants = list(obs_df.index.levels[0])
   for p in participants:
-    comments = ""
+
+    status = participants_df.loc[p]["Participant Status"]
+
+    comments = "Participant status: %s. "%status
     if(len(obs_df[p]))>0:
-      last_date = pd.to_datetime(obs_df[p].index[-1])
-      dates_with_self_report = obs_df[p][obs_df[p]>0]
+      dates = pd.to_datetime(obs_df[p].index)
+      last_date = dates.max()
+      dates_with_self_report = dates[obs_df[p]>0]
       if(len(dates_with_self_report)>0):
-        last_date_with_self_report = pd.to_datetime(dates_with_self_report.index[-1])
+        last_date_with_self_report = dates_with_self_report.max()
         days_since_last_self_report = (last_date - last_date_with_self_report).days
 
         if(days_since_last_self_report>=3):
-          comments = "Requires participant follow-up"
+          if(status=="active"):
+            comments = comments + "Requires participant follow-up."
+          else:
+            days_since_last_self_report = np.nan
       else:
-        last_date_with_self_report = np.nan
+        last_date_with_self_report = pd.NaT
         days_since_last_self_report = np.nan
-        comments = "No recorded self report for this participant"
+        comments = comments + "No recorded self report for this participant"
     else:
-      last_date = np.nan
-      last_date_with_self_report = np.nan
-      days_since_last_self_report = np.nan   
-      comments = "No data found for this participant"
+      last_date = pd.NaT
+      last_date_with_self_report = pd.NaT
+      days_since_last_self_report = np.nan 
+      comments = comments + "No data found for this participant"
 
     response_data.append({"Subject ID":p, 
                           "Last date":last_date,
@@ -50,6 +60,7 @@ def morning_survey_response_report(dc):
                           "Days overdue":days_since_last_self_report,
                           "Comments":comments  
                           })
+    
   df_mis_report = pd.DataFrame(response_data)
   df_mis_report=df_mis_report.set_index("Subject ID")
   df_mis_report.sort_values(by="Days overdue", inplace=True, ascending=False)
