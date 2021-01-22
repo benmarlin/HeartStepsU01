71bccddb7e1d90109a5fcb89502f70a34e2c1a95
karine <76127656+karineML@users.noreply.github.com>
2021-01-22 14:01:18 -0500

    create new linear regression models
    Create new linear regression models.

--- analysis_utils.py
+++ analysis_utils.py
@@ -3,6 +3,9 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
 from . import data_utils
 
 
@@ -31,6 +34,38 @@ def get_correlations(df):
     plt.figure(figsize=(9,8))
     sn.heatmap(correlations, cmap=cm.seismic, annot=True, vmin=-1, vmax=1)
 
+def perform_linear_regression(df):
 
+    df = df.dropna()
+    df = df.reset_index()
 
+    y_names = {'Fitbit Step Count': 'step_count',
+               'Fitbit Minutes Worn': 'minutes_worn'}
 
+    df = df.rename(columns=y_names)
+    df = df.rename(columns={'Subject ID': 'subject_id' })
+    
+    equation  = " ~ Busy + Committed + Rested + Energetic"
+    equation += " + Fatigued + Happy + Relaxed + Sad + Stressed + Tense"    
+
+    for y_display, y_name in y_names.items():        
+        model = y_name + equation
+
+        ind = sm.cov_struct.Exchangeable()
+        mod = smf.gee(model, "subject_id", data=df, cov_struct=ind)    
+        res1 = mod.fit()
+        print('%s =\n%s\n\n\n' % (y_display, res1.summary()))
+
+        mod = smf.mixedlm(model, df, groups="subject_id")    
+        res2 = mod.fit()
+        print('%s =\n%s\n\n\n' % (y_display, res2.summary()))
+
+        df_coef = res1.params.to_frame().rename(columns={0: 'coef'})
+        ax = df_coef.plot.barh(figsize=(14, 7))
+        ax.axvline(0, color='black', lw=1)
+        plt.title(y_display)
+
+        df_coef = res2.params.to_frame().rename(columns={0: 'coef'})
+        ax = df_coef.plot.barh(figsize=(14, 7))
+        ax.axvline(0, color='black', lw=1)
+        plt.title(y_display)
