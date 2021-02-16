45b5407eb9c10ea142493e4503904a063fd7e439
karine <76127656+karineML@users.noreply.github.com>
2021-02-15 22:39:41 -0500

    add regression model in numpyro
    Add regression model in numpyro.
    Refactor to make it more generic.

--- models_numpyro_utils.py
+++ models_numpyro_utils.py
@@ -40,39 +40,33 @@ def plot_data_regression_lines(samples, title, data_x, x_name, data_y, y_name, b
     plt.gcf().tight_layout()
     plt.legend(loc=4)
     if b_show:
-        plt.show()
+        plt.show()    
     plt.close('all')
 
-def model(x1=None, x2=None, x3=None, y_obs=None):
-    a = numpyro.sample('a', dist.Normal(0., 1000))
-    b1x1, b2x2, b3x3 = 0., 0., 0.
-    if x1 is not None:
-        b1 = numpyro.sample('b1', dist.Normal(0., 10.))
-        b1x1 = b1 * x1
-    if x2 is not None:
-        b2 = numpyro.sample('b2', dist.Normal(0., 10.))
-        b2x2 = b2 * x2
-    if x3 is not None:
-        b3 = numpyro.sample('b3', dist.Normal(0., 10.))
-        b3x3 = b3 * x3
-    log_sigma = numpyro.sample('log_sigma', dist.Normal(0., 10.))
-    mu = a + b1x1 + b2x2 + b3x3
+def model(xs=None, y_obs=None):
+    mu = numpyro.sample('a', dist.Normal(0., 1000))
+    M = xs.shape[1]  
+    for i in range(M):
+        b_name = 'b' + str(i+1)
+        bi = numpyro.sample(b_name, dist.Normal(0., 10.))
+        bxi = bi * xs[:,i]
+        mu = mu +  bxi        
+    log_sigma = numpyro.sample('log_sigma', dist.Normal(0., 10.))    
     numpyro.sample('obs', dist.Normal(mu, jnp.exp(log_sigma)), obs=y_obs)
 
 def fit_simple_regression_model_numpyro(df_data, y_name, x_names, b_show=True):
     for x_name in x_names:
-        title = y_name + ' vs ' + str(x_names)
+        title = y_name + ' vs ' + x_name
         print('fitting for %s...' % title)        
         N = 1000
-        data_x    = df_data[x_name].values[:N]
+        data_x = jnp.array([df_data[x_name].values[:N]]).reshape(-1,1)
         data_y = df_data[y_name].values[:N]
 
         #Fit model using NUTS
         rng_key = random.PRNGKey(0)
-        rng_key, rng_key_ = random.split(rng_key)
         kernel = NUTS(model)
         mcmc = MCMC(kernel, num_warmup=1000, num_samples=2000)
-        mcmc.run(rng_key_, x1=data_x, y_obs=data_y)
+        mcmc.run(rng_key, xs=data_x, y_obs=data_y)
 
         #Display summary
         print('summary for %s =' % title)
@@ -82,15 +76,39 @@ def fit_simple_regression_model_numpyro(df_data, y_name, x_names, b_show=True):
         ss = samples['sigma']
         print('sigma mean = %.2f\tstd = %.2f\tmedian = %.2f\tQ5%% = %.2f\tQ95%% = %.2f' % (
               np.mean(ss), np.std(ss), np.median(ss), np.quantile(ss, 0.05, axis=0), np.quantile(ss, 0.95, axis=0)))
-
+        
         #Plot
         plot_data_regression_lines(samples, title, data_x, x_name, data_y, y_name, b_show)
         print('\n\n\n')
+
+def fit_regression_model_numpyro(df_data, y_name, x_names, b_show=True):    
+    title = y_name + ' vs ' + str(x_names)
+    print('fitting for %s...' % title)        
+    N = 1000
+    data_xs = df_data[x_names].values[:N]
+    data_y = df_data[y_name].values[:N]
+
+    #Fit model using NUTS
+    rng_key = random.PRNGKey(0)
+    kernel = NUTS(model)
+    mcmc = MCMC(kernel, num_warmup=1000, num_samples=2000)
+    mcmc.run(rng_key, xs=data_xs, y_obs=data_y)
+
+    #Display summary
+    print('summary for %s =' % title)
+    mcmc.print_summary()
+    samples = mcmc.get_samples()
+    samples['sigma'] = jnp.exp(samples['log_sigma'])    
+    ss = samples['sigma']
+    print('sigma mean = %.2f\tstd = %.2f\tmedian = %.2f\tQ5%% = %.2f\tQ95%% = %.2f' % (
+          np.mean(ss), np.std(ss), np.median(ss), np.quantile(ss, 0.05, axis=0), np.quantile(ss, 0.95, axis=0)))
+    print('\n\n\n')
 
 if __name__ == '__main__':
     test_df = pd.read_csv('test_df.csv')    #Replace with desired test_df
     x_names = ['Committed', 'Busy', 'Rested']
     y_name = 'Fitbit Step Count'
-    fit_simple_regression_model_numpyro(test_df , y_name, x_names, b_show=False)    
+    fit_simple_regression_model_numpyro(test_df , y_name, x_names, b_show=False)
+    fit_regression_model_numpyro(test_df , y_name, x_names, b_show=True)
     print('finished!')
 
