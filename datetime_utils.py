79278271c80d3158f3c6e1c2f83dfb341f4638b4
karine <76127656+karineML@users.noreply.github.com>
2021-08-15 20:25:17 -0400

    update week of
    Update week of:
    + df_d is the data frame that contains index level "d".
    + df_w is the data frame that contains index level "w".

--- datetime_utils.py
+++ datetime_utils.py
@@ -2,14 +2,11 @@ import pandas as pd
 from datetime import date
 from datetime import datetime
 
-def get_date(str_date):
+def get_date(str_date):  
   str_date = datetime.strptime(str_date, '%m/%d/%Y').strftime('%Y,%m,%d')
   check_date = str_date.split(',')
   check_date = date(int(check_date[0]),int(check_date[1]),int(check_date[2]))
   return check_date
-
-def weekof(str_date):  
-  return get_date(str_date).isocalendar()[1]
 
 def dayofweek(str_date):
   day_names = ["ISO week days start from 1", "Monday", "Tuesday",
@@ -23,22 +20,19 @@ def isweekday(str_date):
     b_weekday = False
   return b_weekday
 
-def iweekof(df, previous_levels, str_date):
-  #iweekof find the corresponding week for str_date, then find the corresponging 
-  #index for the week in the data frame df, given previous_levels  
-  week = weekof(str_date)
-  desired_level = len(previous_levels)
-  index_names = df.index.names
-  index_values = {}
-  previous_level_names = []
-  previous_level_value = []
-  for i, index_name in enumerate(index_names):
-    index_values[index_name] = list(df.index.unique(index_name))
-    if i < desired_level:
-      previous_level_names.append(index_name)
-      previous_level_value.append(previous_levels[i])
-  possible_values = index_values[index_names[desired_level]]
-  found_index = -1  
+def weekof(df_d, df_w, d_date):
+  #weekof finds the corresponding week for date
+  #df_d is the data frame that contains index level "d"
+  #df_w is the data frame that contains index level "w" 
+  possible_values = df_d.index.unique(level="d")
+  if d_date < len(possible_values):
+    str_date = possible_values[d_date]
+  else:
+    print('cannot find corresponding date for index', d_date)
+    return -1
+  week = get_date(str_date).isocalendar()[1]
+  found_index = -1
+  possible_values = df_w.index.unique(level="w")
   for i, value in enumerate(possible_values):
     if value == week:
       found_index = i
